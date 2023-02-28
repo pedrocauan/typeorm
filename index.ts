257562d046ccfaa92a271a79658bd7e73392e49a
pedrocauan <mainyasuo92@gmail.com>
2023-02-28 19:03:15 -0300

    Criando a primeira rota

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
-function soma(num: number){
-    return num + 1
-}
+import express, { Request, Response } from "express";
 
-console.log(soma("blablabal"))
+const server = express();
+const PORT = 5004;
+
+server.get("/", (req: Request, res: Response) => {
+    //FUNCIONALIDADES DA ROTA
+    return res.json({ mensagem: "Bem vindo a nossa DIO API" });
+});
+
+server.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
+
+//VERBOS HTTP
+
+/**
+ * GET -> LER OS DADOS    
+ * PUT/PATH -> ATUALIZA OS DADOS
+ * UPDATE -> ATUALIZA OS DADOS
+ * DELETE -> DELETA OS DADOS
+ * 
+ *  */ 
